166c4fd7b234c4c10663d6434d06014d05095790
HamzaThepixelpro <ha444777ha@gmail.com>
2024-07-18 16:14:01 +0500

    Ts At React_01

---
+++ Ts-React_01/src/App.tsx
@@ -0,0 +1,63 @@
+// import { useState } from 'react'
+// import './App.css'
+
+
+
+type Person = {
+  name: string;
+  Age?: number;
+  Address?: string | number;
+}
+let Student: Person = {
+  name: "hhcc",
+}
+console.log(Student.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function App() {
+return (
+  <h1 className="text-3xl ">
+  Hello world!
+  </h1>
+  )
+}
+
+export default App
